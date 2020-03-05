877adca4baac045e18ccc558f19b676d228b2640
Arvin Sevilla <asevilla@yondu.com>
2020-03-05 10:24:18 +0800

    Firefox weird not loading issues

--- test/main.spec.js
+++ test/main.spec.js
@@ -2,21 +2,26 @@ const playwright = require("playwright-core");
 
 (async function () {
     // const browserType = "chromium"
-    const browserType = "webkit"
+    const browserType = "firefox"
+    // const browserType = "webkit"
 
     // await playwright[browserType].downloadBrowserIfNeeded()
     const browser = await playwright[browserType].launch({
-        // headless: false
-        headless: true
+        headless: false
+        // headless: true
     });
     const context = await browser.newContext();
     const page = await context.newPage();
+    console.log("before goto")
     await page.goto('http://localhost:8000');
+    console.log("before button")
     await page.click("button")
+    console.log("after button")
 
     const mainFrame = page.mainFrame()
     const childFrame = mainFrame.childFrames()[0]
 
+    console.log("before")
     await page.screenshot({
         path: "1before.png",
         fullPage: true,
@@ -30,6 +35,7 @@ const playwright = require("playwright-core");
         path: "2after.png",
         fullPage: true,
     })
+    console.log("after")
 
     // await browser.close();
 })();
